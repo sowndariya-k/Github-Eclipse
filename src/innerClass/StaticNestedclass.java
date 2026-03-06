0eb3c67752b86eda9023ffb31781913d0c7ee3dd
sowndariya <sowndariyadeveloper@gmail.com>
2026-03-06 10:44:12 +0530

    type2_static nested class

---
+++ src/innerClass/StaticNestedclass.java
@@ -0,0 +1,30 @@
+//illustrate static nested classes
+package innerClass;
+
+class Person{
+	int age=21;
+	static int weight=45;
+	static class Gender{
+		void nonStaticDisplay() {
+			System.out.println("In NonStatic Display Method");
+			System.out.println("Weight Value: "+weight);//outer class static variable accessed in nonstatic method
+		}
+	
+	
+	static void staticDisplay() {
+		System.out.println("In static Display Method");
+		System.out.println("Weight value: "+weight);//outer class static variable accessed in static method 
+	}
+	}
+	Gender gender=new Gender();
+}
+
+public class StaticNestedclass {
+
+	public static void main(String[] args) {
+		Person person=new Person();
+		person.gender.nonStaticDisplay();
+		person.gender.staticDisplay();
+	}
+
+}
