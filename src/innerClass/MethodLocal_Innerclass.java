7cd3f8431fdfe945e03bc00e63d6c5208183a92f
sowndariya <sowndariyadeveloper@gmail.com>
2026-03-06 10:54:13 +0530

    Method Local innerclass

---
+++ src/innerClass/MethodLocal_Innerclass.java
@@ -0,0 +1,27 @@
+//illustrate method local inner class
+package innerClass;
+
+class Outer{
+	int value=56;
+	public void outerClassMethod() {
+		System.out.println("Inside the Method of Outer class");
+		class Inner{
+			public void innerClassMethod() {
+				System.out.println("Inside the Method of Inner Class");
+				System.out.println("Value: "+value);
+			}
+		}
+		Inner inner=new Inner();
+		inner.innerClassMethod();
+	}
+}
+
+
+public class MethodLocal_Innerclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
